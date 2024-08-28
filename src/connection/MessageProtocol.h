3bbbc082ccc9307ea5ff8946dd8a19ec69082ef9
John Luong <scorpionf904@gmail.com>
2024-08-27 20:16:10 -0400

    implemented MessageProtocol.h

---
+++ src/connection/MessageProtocol.h
@@ -0,0 +1,28 @@
+#include "Util.h"
+
+#include <cstdint>
+
+#ifndef MESSAGEPROTOCOL_H_
+#define MESSAGEPROTOCOL_H_
+
+typedef struct
+{
+	uint16_t magicWord;
+	uint16_t id;
+	uint16_t length;
+
+} MessageHeader_t;
+
+typedef struct
+{
+	MessageHeader_t messageHeader;
+	StaticBoard_t board;
+
+} BoardMessage_t;
+
+void sendBoard(const StaticBoard_t& board);
+void sendACK();
+void sendMove(const Move_t& move);
+void requestMove();
+
+#endif
